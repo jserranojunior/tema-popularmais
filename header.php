7f532df6c610249618fc3eae48024860ef9e0d23
jserranojunior <jorgeserranojunior@hotmail.com>
2018-10-15 14:54:12 -0300

    Tema wordpress

---
+++ header.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+        crossorigin="anonymous">
+
+
+    <link rel="stylesheet" href="<?php bloginfo("template_directory"); ?>/css/style.css">
+
+    <title>Popular Mais</title>
+
+</head>
+
+<body>
+    <div class="container">
+
+
+        <!-- <div class="row bg-danger">
+            Teste
+        </div> -->
+        <section id="menu">
+            <div class="row">
+                <div class="col">
+                    <nav class="navbar shadow-sm navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="#">
+                            <img src="<?php bloginfo("template_directory"); ?>/img/logo-popular-mais.png" alt="Popular Mais Logo" width="150px">
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse align-items-center" id="navbarSupportedContent">
+                            <ul class="navbar-nav">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="/">INÍCIO<span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="http://popularmais.com.br/category/geral/">NOTÍCIAS</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="http://popularmais.com.br/category/politica/">POLITICA</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="http://popularmais.com.br/category/cidade/">CIDADE</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="http://popularmais.com.br/colunistas/">COLUNISTAS</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="http://popularmais.com.br/#">SERVIÇOS</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="http://popularmais.com.br/expediente/">EXPEDIENTES</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </section>
