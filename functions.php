196b90cbb93cbd0410df6375b9ce76fbdd3956a7
jserranojunior <jorgeserranojunior@hotmail.com>
2018-12-03 13:18:02 -0200

    if thumb

--- functions.php
+++ functions.php
@@ -1,1 +1,5 @@
-add_theme_support( ‘post-thumbnails’ );
+// Thumbnails
+
+    if ( function_exists( 'add_theme_support' ) )
+        add_theme_support( 'post-thumbnails' );
+    set_post_thumbnail_size( 400, 400, true );
